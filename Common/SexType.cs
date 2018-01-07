f489f6dc87a350508bc4f85ed12b0725a40dd0cb
Wingkou Loeng <961574992@qq.com>
2018-01-07 22:36:40 +0800

    emmm

--- Common/SexType.cs
+++ Common/SexType.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Common
+﻿namespace Common
 {
     public enum SexType
     {
         Male,
         Female,
-        Unknown // 为了扩展性考虑……
+        Unknown // 为了扩展性考虑
     }
 }
